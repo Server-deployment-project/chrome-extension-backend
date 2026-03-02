c5850aef76f4537f2dd7e93abc250ae93a63ae6f
lishuo <showlee912@outlook.com>
2026-03-02 13:57:25 +0800

    修复注册字段不一致

--- src/main/java/com/extension/backend/dto/RegisterRequest.java
+++ src/main/java/com/extension/backend/dto/RegisterRequest.java
@@ -1,5 +1,7 @@
 package com.extension.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 /**
@@ -7,10 +9,11 @@ import lombok.Data;
  */
 @Data
 public class RegisterRequest {
-    
+
     private String email;
-    
+
     private String password;
-    
+
+    @JsonProperty("confirm_password")
     private String confirmPassword;
 }
